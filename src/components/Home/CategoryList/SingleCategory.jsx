e758d2750470b6cbf45a51814e032f5a33f40fea
Sazidul Alam <sazidulalam20@gmail.com>
2024-03-06 14:43:08 +0600

    category list center

--- src/components/Home/CategoryList/SingleCategory.jsx
+++ src/components/Home/CategoryList/SingleCategory.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 
 const SingleCategory = ({ categoryListItem }) => {
     return (
-        <div className="bg-[#f9f9ff] p-6 rounded-lg">
+        <div className="bg-[#f9f9ff] p-6 rounded-lg flex flex-col items-center md:items-start">
             <div className="bg-[#efecff] w-16 h-16 rounded-lg flex justify-center items-center mb-4">
                 <img src={categoryListItem.logo} alt="accounts" />
             </div>
